6d53e02a575dd763c630df41e765d42dd6cb805c
Scott Fennie <s.fennie7@gmail.com>
2021-09-10 14:13:07 -0600

    bg model created

---
+++ app/Models/BgImage.js
@@ -0,0 +1,6 @@
+export class BgImage {
+    constructor(imageData) {
+        this.imgUrl = imageData.imgUrl
+        this.tags = imageData.tags
+    }
+}
---
+++ app/Services/BackgroundService.js
@@ -0,0 +1,17 @@
+const bgImage = axios.create({
+    baseURL: 'https://bcw-sandbox.herokuapp.com/api/images'
+})
+
+class BackgroundService {
+
+
+    constructor() {
+
+    }
+
+
+}
+
+
+
+export const backgroundService = new BackgroundService()
