038bf24308cc05bc0b5b2909e96dc59957402b8c
Scott Fennie <s.fennie7@gmail.com>
2021-09-12 13:36:50 -0600

    todo updated

--- app/Models/TodoItem.js
+++ app/Models/TodoItem.js
@@ -11,11 +11,9 @@ export class TodoItem {
     get Template() {
 
         return /*html*/ `
-        <ul class="list-group">
         <li class="list-group-item glassy2">
             <input class="form-check-input me-1" type="checkbox" value="" aria-label="..."${this.completed == true ? "checked" : ""}>${this.description}
         </li>
-        </ul>
         `
     }
 }
